3cf77433df80692398a4357c7155d036b8ddf74e
andrewray <evilkidder@gmail.com>
2015-01-26 00:41:13 +0000

    control rendering order of waves in hardcaml sims

--- src/sim.ml
+++ src/sim.ml
@@ -5,31 +5,54 @@ module Make(B : Comb.S)(W : Wave.W with type elt = B.t) = struct
 
   open Cyclesim.Api
 
-  let wrap sim = 
+  let wrap ?cfg sim = 
     
     let cycle = ref 0 in
 
-    let clock = W.Clock "clock", (fun _ -> ()) in
-    let reset = 
-      let d = W.make () in
-      W.Binary("reset", d), (fun v -> W.set d !cycle (if v then B.vdd else B.gnd))
+    let compare =
+      match cfg with
+      | None -> compare
+      | Some(s) -> 
+          let f = 
+            let s = List.mapi (fun i (n,_) -> n,i) s in
+            (fun x -> try Some(List.assoc x s) with _ -> None)
+          in
+          (fun a b ->
+            match f a, f b with
+            | None, None -> compare a b
+            | Some _, None -> -1
+            | None, Some _ -> 1
+            | Some(a), Some(b) -> compare a b)
     in
 
-    let port (n,v) =
-      let d = W.make () in
-      let wave = 
-        if B.width !v = 1 then W.Binary(n, d)
-        else W.Data(n, d, W.B)
-      in
-      wave, (fun _ -> W.set d !cycle !v)
+    let get_type = function
+      | None -> (fun _ -> W.B)
+      | Some(l) -> (fun n -> try List.assoc n l with _ -> W.B)
+    in
+
+    let port (n,v) = 
+        match n with
+        | "clock" | "clk" -> W.Clock n, (fun _ -> ())
+        | "reset" | "rst" -> 
+          let d = W.make() in 
+          W.Binary(n, d), (fun v -> W.set d !cycle (if v then B.vdd else B.gnd))
+        | _ -> 
+          let t = get_type cfg n in
+          let d = W.make () in
+          let wave = 
+            if B.width !v = 1 && t = W.B then W.Binary(n, d)
+            else W.Data(n, d, t)
+          in
+          wave, (fun _ -> W.set d !cycle !v)
     in
 
     let ports = List.concat [
-      [ clock; reset ];
       List.map port (in_ports sim);
       List.map port (out_ports sim);
       List.map port (internal_ports sim); ]
     in
+    
+    let ports = List.sort (fun (w0,_) (w1,_) -> compare (W.get_name w0) (W.get_name w1)) ports in
 
     let waves = Array.of_list (List.map fst ports) in
     let updates = Array.of_list (List.map snd ports) in
--- src/sim.mli
+++ src/sim.mli
@@ -2,7 +2,9 @@ open HardCaml
 
 module Make(B : Comb.S)(W : Wave.W with type elt = B.t) : sig
 
-  val wrap : B.t Cyclesim.Api.cyclesim -> 
+  val wrap :
+    ?cfg:(string * W.to_str) list ->
+    B.t Cyclesim.Api.cyclesim -> 
     B.t Cyclesim.Api.cyclesim * W.wave array
 
 end
